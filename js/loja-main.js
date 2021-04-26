f674946190a119bff38d7173b77fb5df8fad8398
Hugo Leonardo <59965105+hugolps@users.noreply.github.com>
2021-04-25 23:03:57 -0300

    implementada remoção do carrinho

--- js/loja-main.js
+++ js/loja-main.js
@@ -4,6 +4,8 @@ const camisasArray = camisas
 const itensCarrinho = []
 
 function loadCamisas() {
+
+  
   for (const camisa of camisasArray) {
     createCamisasView(camisa)
   }
@@ -16,33 +18,58 @@ const camisasView = `
     <div class="principal-camisa-nome camisa-item"><h1>${camisa.modelo}<h1></div>
     <div class="principal-camisa-preco"><p>Preço: R$${camisa.preco.toFixed(2)}</p></div>
     <div class="camisa-item">
-      <button class="principal-camisa-botao" id=${camisa.id}>ADICIONAR</button>
-    </div>  
-    `
+      <button class="principal-camisa-botao adicionar-camisa" id="${camisa.id}">ADICIONAR</button>
+    </div>
+    <div class="camisa-item">
+      <button class="principal-camisa-botao remover-camisa" id="${camisa.id}">REMOVER</button>
+    </div> `
 
     const camisasDeck = document.querySelector('.principal-camisas');
-
     camisasDeck.insertAdjacentHTML('beforeend', camisasView);
     return camisa.modelo
   }
 
-
 function adicionarNoCarrinho() {
+  console.log(itensCarrinho)
   const idItem = this.id
+  console.log(idItem)
   itensCarrinho.push(camisasArray[idItem - 1])
   fillTotal()
+  console.log(itensCarrinho)
+
 }
 
-loadCamisas()
-const botoes = document.querySelectorAll('.principal-camisa-botao')
+function removerDoCarrinho() {
+  console.log(itensCarrinho)
+  const idItem = this.id
+  
+  for (let i = 0; i < itensCarrinho.length; i++) {
+    if (itensCarrinho[i].id == idItem) {
+      itensCarrinho.splice(i, 1)
+      break
+    }
+  }
 
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].addEventListener('click', adicionarNoCarrinho)
+  fillTotal()
+  console.log(itensCarrinho)
+
+  }
+
+loadCamisas()
+
+const botoesAdicionar = document.querySelectorAll('.adicionar-camisa')
+const botoesRemover = document.querySelectorAll('.remover-camisa')
+
+for (let i = 0; i < botoesAdicionar.length; i++) {
+  botoesAdicionar[i].addEventListener('click', adicionarNoCarrinho)
+}
+
+for (let i = 0; i < botoesRemover.length; i++) {
+  botoesRemover[i].addEventListener('click', removerDoCarrinho)
 }
 
 
 function fillTotal() {
-
   const totalView = `${somaCarrinho().toFixed(2)}`
   const total = document.querySelector('.total-carrinho')
   total.innerHTML = totalView
@@ -50,12 +77,9 @@ function fillTotal() {
   const quantidadeItensView = `${itensCarrinho.length}`
   const quantidadeItens = document.querySelector('.quantidade-itens')
   quantidadeItens.innerHTML = quantidadeItensView
-
-
 }
 
 function somaCarrinho() {
-  console.log('teste somaCarrinho')
 
   const totalCarrinho = itensCarrinho.map(item => item.preco).reduce((acc, atual) => acc + atual, 0)
 
